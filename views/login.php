c4ad9013729aaab9a42904f92035cce294e7e806
Rhomancode <roman.pons31@gmail.com>
2023-02-02 18:43:08 +0100

    Création form login + css

---
+++ views/login.php
@@ -0,0 +1,33 @@
+<?php
+
+$title = "Connectez-vous";
+
+ob_start();
+?>
+<form class="form" action="" method="post">
+    <div class="formGroup">
+        <label for="email">Email</label>
+    </div>
+    <div class="formGroup">
+        <input type="email" name="email" id="email" class="formInput" placeholder="Email" required="required">
+    </div>
+    <div class="formGroup">
+        <label for="email">Mot de passe</label>
+    </div>
+    <div class="formGroup">
+        <input type="password" class="formInput" placeholder="Mot de passe" required="required">
+    </div>
+    <div class="formGroup">
+        <button type="submit" class="formBtn">Se connecter</button>
+    </div>
+    <div class="formGroup">
+    <a href="#" class="formLink">Inscrivez-vous</a>
+    <a href="#" class="formLink">Mot de passe oublié</a>
+    </div>
+</form>
+
+
+
+<?php
+$content = ob_get_clean();
+require('layout.php');
